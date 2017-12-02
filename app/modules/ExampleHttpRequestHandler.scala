c30f4ae04d4fc33434fc44b9b46d144d87e5486f
letusfly85 <letusfly95@gmail.com>
2017-12-02 12:47:50 +0900

    add example request handler

---
+++ app/modules/ExampleHttpRequestHandler.scala
@@ -0,0 +1,28 @@
+package modules
+
+import javax.inject.Inject
+
+//import akka.routing.Router
+import play.api.Logger
+import play.api.http._
+import play.api.mvc.RequestHeader
+import router.Routes
+
+class ExampleHttpRequestHandler @Inject()(errorHandler: HttpErrorHandler,
+                                          configuration: HttpConfiguration, filters: HttpFilters,
+                                          //fooRouter: foo.Routes, barRouter: bar.Routes
+                                          router: Routes
+                                          ) extends DefaultHttpRequestHandler (
+  //fooRouter,
+  router, errorHandler, configuration, filters
+) {
+
+  override def routeRequest(request: RequestHeader) = {
+    Logger.logger.info(request.host)
+    request.host match {
+      //case "foo.example.com" => fooRouter.routes.lift(request)
+      //case "bar.example.com" => barRouter.routes.lift(request)
+      case _ => super.routeRequest(request)
+    }
+  }
+}
